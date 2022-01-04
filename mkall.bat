8eed63d3cb5917963cd952fc5865311379027418
frank manig <fm@dresden-fx.de>
2022-01-04 23:33:37 +0100

    Added scripts for linux and windows to build all targets.

---
+++ mkall.bat
@@ -0,0 +1,25 @@
+@echo off
+
+call mk\setenv.bat %0
+rem set CROSSCOMPILE=C:\dvpt\tools\SourceryARM-EABI-2014.05\bin\arm-none-eabi-
+rem set CROSSCOMPILE=C:\dvpt\tools\arduino-1.8.10\hardware\tools\arm\bin\arm-none-eabi-
+set CROSSCOMPILE=C:\dvpt\tools\gcc-linaro-7.5.0-2019.12-i686-mingw32_arm-eabi\bin\arm-eabi-
+
+echo ######## Report of MKALL.BAT ########                         2>&1 | tee    report.txt
+echo;                                                              2>&1 | tee -a report.txt
+
+echo #### Building startup\flash-boot ####                         2>&1 | tee -a report.txt
+echo;                                                              2>&1 | tee -a report.txt
+make -j -C .\driver\startup\imxrt\flash-boot PLATFORM=IMXRT clean  2>&1 | tee -a report.txt
+make -j -C .\driver\startup\imxrt\flash-boot PLATFORM=IMXRT version bin 2>&1 | tee -a report.txt
+echo;                                                              2>&1 | tee -a report.txt
+echo;                                                              2>&1 | tee -a report.txt
+
+echo #### Building startup\flash-app #####                         2>&1 | tee -a report.txt
+echo;                                                              2>&1 | tee -a report.txt
+rem make -j -C .\driver\startup\imxrt\flash-app PLATFORM=IMXRT clean   2>&1 | tee -a report.txt
+rem make -j -C .\driver\startup\imxrt\flash-app PLATFORM=IMXRT version bin     2>&1 | tee -a report.txt
+echo;                                                              2>&1 | tee -a report.txt
+echo;                                                              2>&1 | tee -a report.txt
+
+pause
---
+++ mkall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CROSSCOMPILE=arm-none-eabi-
+
+echo "######## Report of MKALL.SH #########"                       2>&1 | tee    report.txt
+echo ""                                                            2>&1 | tee -a report.txt
+
+echo "#### Building startup\flash-boot ####"                       2>&1 | tee -a report.txt
+echo ""                                                            2>&1 | tee -a report.txt
+make -j -C ./driver/startup/imxrt/flash-boot PLATFORM=IMXRT clean  2>&1 | tee -a report.txt
+make -j -C ./driver/startup/imxrt/flash-boot PLATFORM=IMXRT version hex 2>&1 | tee -a report.txt
+echo ""                                                            2>&1 | tee -a report.txt
+echo ""                                                            2>&1 | tee -a report.txt
+
