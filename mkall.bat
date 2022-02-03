53c87abfd1e35a8f0c6090e505c31d91a5518a0b
frank manig <fm@dresden-fx.de>
2022-02-04 00:32:45 +0100

    Clenaup build scripts.

--- mkall.bat
+++ mkall.bat
@@ -1,9 +1,6 @@
 @echo off
 
 call mk\setenv.bat %0
-rem set CROSSCOMPILE=C:\dvpt\tools\SourceryARM-EABI-2014.05\bin\arm-none-eabi-
-rem set CROSSCOMPILE=C:\dvpt\tools\arduino-1.8.10\hardware\tools\arm\bin\arm-none-eabi-
-set CROSSCOMPILE=C:\dvpt\tools\gcc-linaro-7.5.0-2019.12-i686-mingw32_arm-eabi\bin\arm-eabi-
 
 echo ######## Report of MKALL.BAT ########                         2>&1 | tee    report.txt
 echo;                                                              2>&1 | tee -a report.txt
@@ -13,7 +10,7 @@ echo;                                                              2>&1 | tee -a
 make -j -C .\demo\startup\imxrt\flash-boot PLATFORM=IMXRT clean    2>&1 | tee -a report.txt
 make -j -C .\demo\startup\imxrt\flash-boot PLATFORM=IMXRT version  2>&1 | tee -a report.txt
 make -j -C .\demo\startup\imxrt\flash-boot PLATFORM=IMXRT hex      2>&1 | tee -a report.txt
-rem make -j -C .\demo\startup\imxrt\flash-boot PLATFORM=IMXRT postlink 2>&1 | tee -a report.txt
+make -j -C .\demo\startup\imxrt\flash-boot PLATFORM=IMXRT postlink 2>&1 | tee -a report.txt
 echo;                                                              2>&1 | tee -a report.txt
 echo;                                                              2>&1 | tee -a report.txt
 
--- mkall.sh
+++ mkall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CROSSCOMPILE=arm-none-eabi-
+source mk/setenv.sh
 
 echo "######## Report of MKALL.SH #########"                       2>&1 | tee    report.txt
 echo ""                                                            2>&1 | tee -a report.txt
@@ -10,7 +10,7 @@ echo ""                                                            2>&1 | tee -a
 make -j -C ./demo/startup/imxrt/flash-boot PLATFORM=IMXRT clean    2>&1 | tee -a report.txt
 make -j -C ./demo/startup/imxrt/flash-boot PLATFORM=IMXRT version  2>&1 | tee -a report.txt
 make -j -C ./demo/startup/imxrt/flash-boot PLATFORM=IMXRT hex      2>&1 | tee -a report.txt
-# make -j -C ./demo/startup/imxrt/flash-boot PLATFORM=IMXRT postlink 2>&1 | tee -a report.txt
+make -j -C ./demo/startup/imxrt/flash-boot PLATFORM=IMXRT postlink 2>&1 | tee -a report.txt
 echo ""                                                            2>&1 | tee -a report.txt
 echo ""                                                            2>&1 | tee -a report.txt
 
